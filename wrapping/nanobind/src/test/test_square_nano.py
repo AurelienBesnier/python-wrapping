f0ac3d1f0fa196be7b5bbad734b3bcd1b0ce4292
AurelienBesnier <aurelienbesnier.pro@gmail.com>
2025-04-16 14:21:33 +0200

    Renaming tests

--- wrapping/nanobind/src/test/test_square_nano.py
+++ wrapping/nanobind/src/test/test_square_nano.py
@@ -2,6 +2,6 @@ import pytest
 from wrapping.nanobind import square
 
 @pytest.mark.benchmark(warmup=True)
-def test_square(benchmark):
+def test_square_nano(benchmark):
     result = benchmark(square, 2.0)
     assert result == 4.0
