a4923a1681730c3962f9e3f0f9f3c76c9980f7a6
AurelienBesnier <aurelienbesnier.pro@gmail.com>
2025-04-23 14:44:00 +0200

    fixing to real

--- wrapping/fortran/src/fortran/ext_fortran2.f90
+++ wrapping/fortran/src/fortran/ext_fortran2.f90
@@ -1,6 +1,6 @@
 subroutine square(i, b)
-    integer, intent (in)  :: i              ! input
-    integer, intent (out) :: b              ! output
+    real, intent (in)  :: i              ! input
+    real, intent (out) :: b              ! output
 
     b = i**2
 end subroutine
