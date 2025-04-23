532a4e7fd027e740814a68e8868c6c23f108ad60
AurelienBesnier <aurelienbesnier.pro@gmail.com>
2025-04-23 16:02:40 +0200

    Adding example of build with setuptools

---
+++ build-systems/setuptools/src/wrapping/setuptools/__init__.py
@@ -0,0 +1,10 @@
+try:
+    from .example import square
+
+    __all__ = ["square"]
+
+except Exception as e:
+    import traceback
+
+    traceback.print_exception(e)
+    print("could not import c++ library")
