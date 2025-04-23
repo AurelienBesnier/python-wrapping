8656eb60a74241519f6c082cdddceccd770df7de
AurelienBesnier <aurelienbesnier.pro@gmail.com>
2025-04-23 14:52:24 +0200

    skipping shell test for benchmark

--- wrapping/shell/test/test_square_shell.py
+++ wrapping/shell/test/test_square_shell.py
@@ -1,7 +1,7 @@
 import pytest
 from wrapping.shell import square
 
-
+@pytest.mark.skip
 @pytest.mark.benchmark(group="square", warmup=True)
 def test_square_shell(benchmark):
     result = benchmark(square, 2.0)
